9dc9c5f1c1d704cbd9bb7c81759f34c45841348a
Dirk-Wilhelm Peters <peters@schwertfisch.de>
2023-11-09 04:06:51 +0330

    dblp.sh: use https for dblp.org

--- dblp.sh
+++ dblp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Query the DBLP database
-curl "http://dblp.org/search/publ/api?q=`echo $1 | sed 's/ /+/g'`&format=json" >dblp.out
+curl "https://dblp.org/search/publ/api?q=`echo $1 | sed 's/ /+/g'`&format=json" >dblp.out
 # Convert search results to refer records
 ./dblp <dblp.out
